cabea28db5a50e4e7f024e7928c8c9ae040fd125
Marya Sharaf <marya.sharaf@tktk.ee>
2024-12-02 02:15:32 +0200

    merging two projects

---
+++ public/scripts/app.js
@@ -0,0 +1,8 @@
+const movieTitle = document.querySelector('.movie-title');
+const releaseDate = document.querySelector('.release-date');
+const movieGenres = document.querySelector('.genres');
+const movieDuration = document.querySelector('.movie-duration');
+const moviePoster = document.querySelector('.movie-poster-container img');
+const movieQuoet = document.querySelector('.movie-info-quote');
+const movieOverview = document.querySelector('.movie-info-overview');
+const footerYear = document.querySelector('.year');
